dd3567783806dae6e4726cd181b44add273186cf
Jeremy Seitz <jeremy@somebox.com>
2024-01-22 06:39:56 +0100

    smooth scrolling attempt (gliches)

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,54 @@
 #include <Arduino.h>
 #include <FastLED.h>
 #include "font.h"
+#include "story.h"
 
-//const char *phrases[] = {
-  // "12345678901234567890",
-  // "ABCDEFGHIJKLMNOPQRST",
-  // "UVWXYZ-+*/=<>!@#$%^&",
-  // "Forest green scent",
-  // "Rich purple reigns",
-  // "Soft pink is sweet",
-  // "Deep aqua seaweeds",
-  // "Creamy white beams",
-  // "Warm brown feeling",
-  // "Cool gray mountain"
-//};
-
-#define NUM_PHRASES 30
 const char *phrases[] = {
-    "A wizard, old&wise,",
-    "cast a spell:'\xAB\xCD'",
-    "to save`the kingdom",
-    "from dragons@ night.",
-    "He said,\"Beware of ",
-    "the dark!\"$%&'()*+,-",
-    "/;<=>?@[\\]^_`{|}~.",
-    "His apprentice, Zed",
-    "listened closely...",
-    "Armed with^courage,",
-    "Zed journeyed_far.",
-    "Mountains, rivers; ",
-    "forests_enchanted.",
-    "Each step: a new`",
-    "challenge. His path",
-    "twisted&turned~<>",
-    "facing fears, hope,",
-    "and despair. 'Till",
-    "at last, the final",
-    "battle_loomed.`",
-    "With magic words:",
-    "\"Fiat lux!\"{he roared",
-    "and light shone out",
-    "Darkness fled away",
-    "Peace returned, and",
-    "Zed became a hero.",
-    "His tale: timeless.",
-    "End of tale... (32-",
-    "127 ASCII chars)~",
-    "The End. ~Wizardry"
+  "12345678901234567890",
+  "ABCDEFGHIJKLMNOPQRST",
+  "UVWXYZ-+*/=<>!@#$%^&",
+  "",
+  "Holy shit, Batman!!!",
+  "<------------------>",
+  "Deep aqua seaweeds",
+  "Creamy white beams",
+  "Warm brown feeling",
+  "Cool gray mountain"
 };
+
+#define NUM_PHRASES 10
+// const char *phrases[] = {
+//     "A wizard, old&wise,",
+//     "cast a spell:'\xAB\xCD'",
+//     "to save`the kingdom",
+//     "from dragons@ night.",
+//     "He said,\"Beware of ",
+//     "the dark!\"$%&'()*+,-",
+//     "/;<=>?@[\\]^_`{|}~.",
+//     "His apprentice, Zed",
+//     "listened closely...",
+//     "Armed with^courage,",
+//     "Zed journeyed_far.",
+//     "Mountains, rivers; ",
+//     "forests_enchanted.",
+//     "Each step: a new`",
+//     "challenge. His path",
+//     "twisted&turned~<>",
+//     "facing fears, hope,",
+//     "and despair. 'Till",
+//     "at last, the final",
+//     "battle_loomed.`",
+//     "With magic words:",
+//     "\"Fiat lux!\"{he roared",
+//     "and light shone out",
+//     "Darkness fled away",
+//     "Peace returned, and",
+//     "Zed became a hero.",
+//     "His tale: timeless.",
+//     "End of tale... (32-",
+//     "127 ASCII chars)~",
+//     "The End. ~Wizardry"
+// };
 
 CRGB phrase_colors[] = {
   CHSV(60, 255, 128),    // Bright yellow
@@ -99,36 +100,76 @@ void color_show(){
 void set_led(uint8_t x, uint8_t y, CRGB color) {
   if (x < NUM_CHARS*5 && y < 7) {
     int offset = x/5*30;
-    // flip each character horizontally
-    // x = (x / 4) + 3 - (x % 4);
-    leds[x+y*5+offset] = color;
+    // as it's a zig-zag pattern, we use the row to adjust x
+    int x2 = x;
+    if (y % 2 == 0){
+      int x_start = floor(x/5.0)*5;
+      x2 = x_start + 4-(x % 5);
+    }
+    leds[y*5+x2+offset] = color;
   }
 }
 
-void write_character(uint8_t character, uint8_t i, CRGB color) {
+void write_character(uint8_t character, uint8_t pos, CRGB color, int offset=0) {
 	// Loop through 7 high 5 wide monochrome font
 	for (int py = 0; py < 7; py++) { // rows
 		for (int px = 0; px < 5; px++) {  // columns
-      int col = (py % 2 == 0) ? px : 4 - px;  
-			if (FONT_BIT(character - 16, px, py)) {        
-				set_led(i*5 + col, py, color);
+			if (FONT_BIT(character - 16, 4-px, py)) {        
+				set_led(pos*5 + px + offset, py, color);
 			} else {
-        set_led(i*5 + col, py, CRGB::Black);
+        set_led(pos*5 + px + offset, py, CRGB::Black);
       }
 		}
 	}
 }
 
+void scroll_message_smooth(String s, int d=20){
+  for (int offset=4; offset>=0; offset--){  
+    FastLED.clear();
+    for (int pos = 0; pos < NUM_CHARS; pos++) {
+      CRGB c = CHSV((millis()/100+pos*3) % 255, 255, 150);
+      char thechar = ' ';
+      if (pos < s.length()){ // access limit by end of string
+        thechar = s.c_str()[pos];
+      }
+      write_character(thechar, pos, c, 5-offset);
+    }
+    FastLED.show();
+    delay(d);
+  }
+}
+
+void write_message(String s, int mode=0){
+  for (int i = 0; i < NUM_CHARS; i++) {
+    char thechar = ' ';
+    if (i < s.length()){ // access limit by end of string
+      thechar = s.c_str()[i];
+    }
+    CRGB c;
+    switch (mode){
+      case 0:
+        c = CHSV((millis()/100+i*3) % 255, 255, 150);
+        break;
+      case 1:
+      case 2:
+        c = phrase_colors[i % 10];
+        break;
+    }
+    if (mode == 2){
+      for (int j=0; j<10; j++){
+        write_character(random(26)+1, i, nblend(c, CRGB(random(255),random(255),random(255)), 5));
+        delay(2+j);
+        FastLED.show();
+      }  
+    }
+    write_character(thechar, i, c);
+  }
+  
+}
 
 void scroll_message(String s, CRGB color){
   for (int p=0; p <= (s.length()); p++){
-    for (int i = 0; i < NUM_CHARS; i++) {
-      char thechar = ' ';
-      if ((i+p) < s.length()){ // access limit by end of string
-        thechar = s.c_str()[i+p];
-      }
-      write_character(thechar, i, color);
-    }
+    write_message(s);
     if (digitalRead(0) == LOW){
       return;
     } else {
@@ -167,7 +208,7 @@ void setup() {
 
 
 int mode = 0;
-
+int spos = 0;
 void loop() {
   static uint8_t character = 1;
   static uint8_t phrase = 0;
@@ -181,7 +222,7 @@ void loop() {
     while (digitalRead(0) == LOW){
       CRGB c = CRGB::White;
       FastLED.setBrightness(50);
-      c.setHSV(millis()/20 % 255, 255, 50);
+      c.setHSV(millis()/100 % 230 + 4, 255, 50);
       FastLED.showColor(c);
       FastLED.show(); 
       delay(10); 
@@ -192,13 +233,29 @@ void loop() {
   if (mode == 0){
     CRGB color = phrase_colors[phrase % 10];
     //String s = news[phrase]["title"];
-    String s = phrases[phrase];
-    scroll_message(s, color);
+    // String s = phrases[phrase];
+
+    // create a string of 20 characters from *story, starting a spos
+    String substring;
+    if (spos >= 0 && spos + 20 <= story.length()) {
+      substring = story.substring(spos, spos+20);
+      spos++;
+    } else {
+      spos = 0;
+    }
+
+    FastLED.clear();
+    //write_message(substring, 0);
+    
+    scroll_message_smooth(substring, 200);
+    //delay(3000);
     // String a = news[phrase]["abstract"];
     // scroll_message(a, color);
 
-    FastLED.clear();
+    //FastLED.clear();
     FastLED.show();
+    delay(500);
+
     phrase++;
     phrase = phrase % NUM_PHRASES;
 
