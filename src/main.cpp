11fea882d98357d9466ea38d38777be6e9874cfb
Jeremy Seitz <jeremy@somebox.com>
2024-02-08 22:52:35 +0100

    refactoring for smoother scrolling, colored words

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <FastLED.h>
 #include "font.h"
 #include "story.h"
+#include <vector>
 
 const char *phrases[] = {
   "12345678901234567890",
@@ -148,33 +149,10 @@ void write_character(uint8_t character, uint8_t pos, CRGB color, int offset=0) {
 	for (int py = 0; py < 7; py++) { // rows
     int adjusted_offset = 0;
 		for (int px = 0; px < 5; px++) {  // columns
-      // if (px + offset == -1 || px + offset == 5) {
-      //   adjusted_offset = offset/abs(offset); // 1 or -1
-      //   continue;
-      // }
-      bool skip = false;
-      switch (offset){
-        case -1:
-          if (px == 0) skip = true;
-          break;
-        case -2:
-          if (px == 1) skip = true;
-          adjusted_offset = (px < 2) ? 1 : 0;
-          break;
-        case -3:
-          if (px == 2) skip = true;
-          adjusted_offset = (px < 3) ? 1 : 0;
-          break;
-        case -4:
-          if (px == 3) skip = true;
-          adjusted_offset = (px < 4) ? 1 : 0;
-          break;
-        case -5:  
-          if (px == 4) skip = true;
-          adjusted_offset = (px < 5) ? 1 : 0;
-          break;
+      if (px == abs(offset)-1) continue;
+      if (offset < -1){
+        adjusted_offset = (px < abs(offset)-1) ? 1 : 0;
       }
-      if (skip) continue;
 			if (FONT_BIT(character - 16, px, py)) {  
 				set_led(pos*5 + px + offset + adjusted_offset, py, color);
 			} else {
@@ -184,15 +162,29 @@ void write_character(uint8_t character, uint8_t pos, CRGB color, int offset=0) {
 	}
 }
 
-void scroll_message_smooth(String s, int scroll_delay){
+
+void scroll_message_smooth(String s, int spos, int scroll_delay){
+  String substring = s.substring(spos, spos+21);
   for (int offset=0; offset>=-5; offset--){  
     //Serial.printf("offset: %d\n", offset);
     FastLED.clear();
     for (int pos = 0; pos <= NUM_CHARS; pos++) {
-      CRGB c = CHSV((millis()/100+pos*3) % 255, 255, 229);
+      int prev_space = 0;
+      // search backwards from current pos+spos in string for a space
+      for (int i = pos+spos; i >= 0; i--){
+        if (s.c_str()[i] == ' '){
+          prev_space = i;
+          break;
+        }
+      }
+      // assign c to the color of the word
+      // CRGB c = phrase_colors[(prev_space+1) % NUM_PHRASES];
+
+      // set color based on prev_space
+      CRGB c = CHSV((prev_space % 25)*(10), 255, 180);
       char thechar = ' ';
       if (pos < s.length()){ // access limit by end of string
-        thechar = s.c_str()[pos];
+        thechar = s.c_str()[spos+pos];
       }
       write_character(thechar, pos, c, offset);
     }
@@ -207,7 +199,7 @@ void write_message(String s, int mode=0){
     if (i < s.length()){ // access limit by end of string
       thechar = s.c_str()[i];
     }
-    CRGB c;
+    CRGB c = CRGB(50,50,50);
     switch (mode){
       case 0:
         c = CHSV((millis()/100+i*3) % 255, 255, 150);
@@ -232,23 +224,13 @@ void write_message(String s, int mode=0){
 
 void story_time(int scroll_delay){
     static int spos = 0;
-    static uint8_t phrase = 0;
-    CRGB color = phrase_colors[phrase % 10];
-    //String s = news[phrase]["title"];
-    // String s = phrases[phrase];
 
-    // create a string of 20 characters from *story, starting a spos
-    String substring;
-    if (spos >= 0 && spos + 20 <= story.length()) {
-      substring = story.substring(spos, spos+20);
+    if (spos >= 0 && spos + 21 <= story.length()) {
       spos++;
     } else {
       spos = 0;
     }
-    scroll_message_smooth(substring, scroll_delay);
-
-    phrase++;
-    phrase = phrase % NUM_PHRASES;
+    scroll_message_smooth(story, spos, scroll_delay);
 }
 
 void headlines(){
@@ -278,7 +260,7 @@ void setup() {
   // set up fastled
   Serial.begin(115200);
   FastLED.addLeds<WS2812B, 5, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(50);
+  FastLED.setBrightness(10);
 
   pinMode(0, INPUT_PULLUP);
 
@@ -311,7 +293,7 @@ void loop() {
 
   switch(mode){
     case 0:
-      story_time(2);
+      story_time(1);
       break;
     case 1:
       test_patterns();
